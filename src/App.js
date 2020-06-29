8b831b4b6eeb72bd9b0447fae6451d701f535935
DarcyLawrence <35388883+DarcyLawrence@users.noreply.github.com>
2020-06-29 15:18:49 -0500

    Init add src files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person'
+
+class App extends Component {
+
+  state = {
+    persons: [
+      { name: "Darcy", age: 27 },
+      { name: "Sergio", age: 25 },
+      { name: "Marcus", age: 26 }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Darcy's React App</h1>
+        <p>Its a test paragraph!</p>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Being Insane</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
+}
+
+export default App;
